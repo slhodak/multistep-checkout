a67ecb21df2df87c465f4290bdea4cebfe3a7da0
Sam Hodak <slhodak@gmail.com>
2019-05-08 11:33:25 -0700

    Create express server

---
+++ public/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CheckoutButton extends React.Component {
+  render() {
+    return(
+      <button>Checkout</button>
+    )
+  }
+}
+
+
